a5dfe31cb6b558adbf5c0c8a24f3557cab33f134
Uday <Uda12@uclive.ac.nz>
2024-02-18 16:05:10 +1300

    basic ui changes

--- ucisa-app/src/App.js
+++ ucisa-app/src/App.js
@@ -1,21 +1,20 @@
-// App.js
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Navbar from "./BasicPageStructure/Navbar";
 import Footer from "./BasicPageStructure/Footer";
 import Main from "./BasicPageStructure/Main";
 import "./App.css";
 
 function App() {
-
-  const [currentPage,setCurrentPage] = useState("Home");
+  const [currentPage, setCurrentPage] = useState("Home");
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
-  }
+  };
+
   return (
-    <div>
+    <div className="app-container">
       <Navbar onPageChange={handlePageChange} />
-      <Main currentPage={currentPage} />
+      <Main currentPage={currentPage} className="main" />
       <Footer />
     </div>
   );
--- ucisa-app/src/Pages/UCISA.jsx
+++ ucisa-app/src/Pages/UCISA.jsx
@@ -13,6 +13,12 @@ function UCISA() {
       <p className="background">
         UCISA stands for University of Canterbury Indian Students Association. It was established in 2022 by Dristhi and others. The club provides a platform for Indian students, both new and old, to come together and build a community, especially for those who have moved to New Zealand, leaving behind their family and friends. UCISA offers opportunities for students to meet new people, participate in events, festivals, sports events, and more.
       </p>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
     </div>
   );
 }
